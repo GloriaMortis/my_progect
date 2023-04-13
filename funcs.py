67fd6d8a46a78b946520119f8242064824121680
GloriaMortis <2x2priboy@gmail.com>
2023-04-13 12:13:56 +0300

    add another function

--- funcs.py
+++ funcs.py
@@ -4,3 +4,11 @@ def plus_number():
     result = numb_1 + numb_2
 
     return result
+
+def minum_number():
+    numb_1 = int(input('введите число :'))
+    numb_2 = int(input('введите число :'))
+    result = numb_1 + numb_2
+
+    return result
+
